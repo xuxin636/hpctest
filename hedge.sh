e42f149336a05d45c1de42e97867719025435bec
xuxin636 <44443100+xuxin636@users.noreply.github.com>
2019-04-29 18:07:10 -0400

    Update hedge.sh

--- hedge.sh
+++ hedge.sh
@@ -2,11 +2,9 @@
 #
 #
 #SBATCH --account=stats      # The account name for the job.
-#SBATCH --job-name=test    # The job name.
+#SBATCH --job-name=hedge    # The job name.
 #SBATCH -c 1                     # The number of cpu cores to use.
 #SBATCH --time=1:00:00              # The time the job will take to run.
 #SBATCH --mem-per-cpu=5gb        # The memory the job will use per cpu core.
-module load R
 
-#Command to execute R code
 R a.R -n routput
